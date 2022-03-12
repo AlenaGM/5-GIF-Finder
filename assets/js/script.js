31457f4f683f859b9babd8ce50383fd4c4f99288
AlenaGM <alena.guillaume4@gmail.com>
2022-03-12 23:57:58 +0100

    style impr

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,23 @@
-function onSearch(){
+document.querySelector('#search').addEventListener('click', function(){
     let gif = document.getElementById("gifSearch").value;
 
 
     fetch('https://api.giphy.com/v1/gifs/search?api_key=YE5Q5b8pJlSFjrfT1KcSWaEQy8KsAdIj&q=' + gif + '&limit=5&offset=0&rating=g&lang=en&fixed_width')
     .then(response => response.json())
     .then(gifka => {
-        document.getElementById("gifView").innerHTML += `<img src="${gifka.data[0].images.fixed_width.url}"></img>`+`<img src="${gifka.data[1].images.fixed_width.url}"></img>`+`<img src="${gifka.data[2].images.fixed_width.url}"></img>`+`<img src="${gifka.data[3].images.fixed_width.url}"></img>`+`<img src="${gifka.data[4].images.fixed_width.url}"></img>`
+        document.getElementById("gifView").innerHTML +=
+        `<img src="${gifka.data[0].images.fixed_width.url}" alt="${gif}" class="img"></img>`+
+        `<img src="${gifka.data[1].images.fixed_width.url}" alt="${gif}" class="img"></img>`+
+        `<img src="${gifka.data[2].images.fixed_width.url}" alt="${gif}" class="img"></img>`+
+        `<img src="${gifka.data[3].images.fixed_width.url}" alt="${gif}" class="img"></img>`+
+        `<img src="${gifka.data[4].images.fixed_width.url}" alt="${gif}" class="img"></img>`
     })
-
-
-
     .catch(error =>console.log(error));
-}
+});
 
 document.querySelector('#reset').addEventListener('click', function(){
     location.reload();
-})
+});
 
 // document.getElementById("gifView").innerHTML += `<img src="${gifka.data[0].images.fixed_width.url}"></img>`
 
